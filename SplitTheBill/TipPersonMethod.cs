a8df3db5ae82f523d290547cc9460f002e6bed3e
Olatunde <122512872+Lawaltunde@users.noreply.github.com>
2024-04-16 15:52:59 +0000

    Second test added

--- SplitTheBill/TipPersonMethod.cs
+++ SplitTheBill/TipPersonMethod.cs
@@ -4,8 +4,12 @@ public class TipPersonMethod{
     public decimal tipPerPerson(decimal price, int patron, float tipPercentage){
 
         var totalPrice = price * patron;
-        
+
         var totalTip = (totalPrice * (decimal)tipPercentage) / 100;
+
+        if(patron == 0){
+            return 0.0M;
+        }
 
         return totalTip/patron;
     }
--- SplitTheBillTest/TipPerPersonTest.cs
+++ SplitTheBillTest/TipPerPersonTest.cs
@@ -6,6 +6,8 @@ using SplitTheBill;
 [TestClass]
 public class TipPersonTest{
     TipPersonMethod tipPersonMethod = new TipPersonMethod();
+
+    [TestMethod]
     public void return_Exact_Value_WhenAll_Parameters_ArePassed(){
         decimal price = 1000.0M; 
         int patrons = 15;
@@ -13,5 +15,15 @@ public class TipPersonTest{
 
         var ret = tipPersonMethod.tipPerPerson(price, patrons, tipercentage);
         Assert.AreEqual(250, (double)ret);
+    }
+
+    [TestMethod]
+    public void return_Zero_if_Patron_Value_IsZero_And_Other_Parameters_ArePassed(){
+        decimal price = 1000.0M; 
+        int patrons = 0;
+        float tipercentage = 25.0f; 
+
+        var ret = tipPersonMethod.tipPerPerson(price, patrons, tipercentage);
+        Assert.AreEqual(0, (double)ret);
     }
 }
